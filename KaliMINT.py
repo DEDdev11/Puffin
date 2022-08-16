7b4e671fc5b910041ded7444fff50488019d008a
DEDdev11 <109730843+DEDdev11@users.noreply.github.com>
2022-08-16 14:59:07 +0100

    Add files via upload

---
+++ KaliMINT.py
@@ -0,0 +1,41 @@
+import random
+import time
+
+print("welcome")
+x=input("username: ")
+input("password: ")
+time.sleep(1)
+print("loading...")
+time.sleep(5)
+print("USER",x,"/ kaliMINT / puffin / home")
+while True:
+ s=input(">>")
+ if s==("/downloads"):
+  time.sleep(3)
+  print("KaliMINT-home.py")
+  print("URLchecker.pme")
+  print("[web]")
+ elif s==("/downloads/web"):
+    print("webopener.web.pme")
+    print("/web.shortcut")
+ elif s==("/documents"):
+   time.sleep(3)
+   print("puffinopenorder.txt")
+ elif s==("/documents/puffinopenorder/txt"):
+    print("puffin is so happy for you to join KaliMINT.")
+ elif s==("/apps"):
+    print("URLchecker")
+ elif s==("/apps(URLchecker)"):
+    print("please write a URL")
+    input("URL please")
+    print("yes this is a correct URL")
+ elif s==("/web"):
+     search=input("webxr ")
+     if search==("KaliMINT"):
+         print("your software being run right now.")
+     elif search==("puffin"):
+         print("a forkable software that wants people to make there own version of")
+     else:
+        print("your search is scratching our heads please enter it again")
+ else:
+  print("error")

0ef93d00e87b93689480902b5a1b9067f83dc0ed
DEDdev11 <109730843+DEDdev11@users.noreply.github.com>
2022-08-16 13:05:57 +0100

    Add files via upload

---
+++ ubu 0.1.9.py
@@ -0,0 +1,50 @@
+import random
+import time
+import datetime
+
+print("welcome")
+x=input("username: ")
+input("password: ")
+time.sleep(1)
+print("loading...")
+time.sleep(5)
+print("USER",x,"/ ubuOS / home")
+while True:
+ s=input(">>")
+ if s==("/downloads"):
+  time.sleep(3)
+  print("UbuEV.py")
+  print("TOOLKIT.eve")
+  print("ubu.txt")
+  print("Terminal.eve")
+ elif s==("/documents"):
+   time.sleep(3)
+   print("ubu.txt")
+ elif s==("/documents/ubu/txt"):
+    print("welcome to ubu the ultimate software for hackers")
+ elif s==("/applications"):
+    time.sleep(3)
+    print("(ubu)TOOLkit.eve")
+    print("terminal(THRE).eve")
+ elif s==("/applications/ubu/TOOLkit/eve"):
+    print("IVE complete your server location is NAT")
+    N = datetime.datetime.now()
+    print(N,"is the Time/date")
+ elif s==("/applications/THRE/terminal/eve"):
+    print("terminal booting")
+    time.sleep(2)
+    print("##~~~###")
+    THRE=input("THRE~~#")
+    if THRE==("sd- start"):
+        print("starting language THRE")
+    elif THRE==("sd- close"):
+        print("ending languagr THRE")
+    elif THRE==("cr- (cryptoc)wallet"):
+        print("do you want a crypto wallet")
+        input("enter to except")
+        time.sleep(0.5)
+        print("your crypto is")
+        time.sleep(0.001)
+        print("0")
+ else:
+  print("error")

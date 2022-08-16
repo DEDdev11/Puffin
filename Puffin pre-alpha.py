4252cfe262635a607cfbd5bb05d6a510d9a92516
DEDdev11 <109730843+DEDdev11@users.noreply.github.com>
2022-08-16 11:39:50 +0100

    Create Puffin pre-alpha.py

---
+++ Puffin pre-alpha.py
@@ -0,0 +1,22 @@
+import random
+import time
+
+print("welcome")
+x=input("username: ")
+input("password: ")
+time.sleep(1)
+print("loading...")
+time.sleep(5)
+print("USER",x,"/home")
+while True:
+ s=input(">>")
+ if s==("/downloads"):
+  time.sleep(3)
+  print("puffinPre-alphaEV.py")
+ elif s==("/documents"):
+   time.sleep(3)
+   print("puffinopenorder.txt")
+  elif s==("/documents/puffinopenorder/txt"):
+    print("puffin is so happy for you to join puffin pre-alpha make sure to Fork our github project to make your own version and then merge your project to make it a downloadable version of puffin thanks puffinPRO cast.")
+ else:
+  print("error")
